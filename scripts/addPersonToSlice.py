285d9bac23eccb8e97b22ee566f1a508a065413d
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-07-26 16:09:26 -0400

    more tests

---
+++ scripts/addPersonToSlice.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import os, time, sys, string, re, traceback
+import xmlrpclib
+
+import func.overlord.client
+
+plc = xmlrpclib.ServerProxy("https://www.planet-lab.org/PLCAPI/", allow_none = True)
+
+auth = {}
+auth["Username"] = "USERNAME"
+auth["AuthString"] = "PASSWORD"
+auth["AuthMethod"] = "password"
+
+node_name = "planetlab-02.cs.princeton.edu"
+slice_name = "princeton_caglar"
+key_id = 20184
+
+try:
+    slice = plc.GetSlices(auth, slice_name)[0]
+    keys = plc.GetKeys(auth, {'person_id': slice["person_ids"]}, ['key', 'key_type'])
+    keys =+ plc.GetKeys(auth, {'person_id': key_id}, ['key', 'key_type'])
+except:
+    keys = []
+
+client = func.overlord.client.Client(node_name, timeout=10, async=True)
+if client.test.ping():
+    client.nm.AddPersonToSlice(slice_name, keys)
+else:
+    print ("Couldn't reach to %s" % node_name)

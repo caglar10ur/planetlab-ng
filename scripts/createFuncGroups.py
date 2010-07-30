3e5bed7ad95d33f367849c6908860436a98232a6
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-07-30 11:06:28 -0400

    add scripts/createFuncGroups.py and adjust config files according to latest changes

---
+++ scripts/createFuncGroups.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import xmlrpclib
+
+plc = xmlrpclib.ServerProxy('https://www.planet-lab.org/PLCAPI/', allow_none = True)
+
+auth = {}
+auth['Username'] = "caglar@cs.princeton.edu"
+auth['Username'] = "USERNAME"
+auth['AuthString'] = "PASSWORD"
+
+if __name__ == "__main__":
+        sites = plc.GetSites(auth, {'enabled': True})
+        for site in sites:
+                nodes = [node["hostname"] for node in plc.GetNodes(auth, site["node_ids"])]
+                print "[%s]\nhost = %s\n" % (site["login_base"], ",".join(nodes))
+
